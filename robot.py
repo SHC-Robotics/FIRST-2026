da47cc0a5214f55315344dac8dd0411b1d88b989
Killian Hourihan <kllnhrhn@gmail.com>
2026-01-15 14:55:32 -0800

    Add DifferentialDrive system with arcade steering
    Uses wpilib builtin DifferentialDrive class

--- robot.py
+++ robot.py
@@ -1,1 +1,15 @@
-# TODO: insert robot code here
+import wpilib
+import wpilib.drive
+
+
+class Robot(wpilib.TimedRobot):
+    def robotInit(self):
+        leftMotor = wpilib.PWMSparkMax(0)
+        rightMotor = wpilib.PWMSparkMax(1)
+        self.robotDrive = wpilib.drive.DifferentialDrive(leftMotor, rightMotor)
+        self.stick = wpilib.Joystick(0)
+
+        rightMotor.setInverted(True)
+
+    def teleopPeriodic(self):
+        self.robotDrive.arcadeDrive(self.stick.getY(), self.stick.getX())
