d0a115ded31db77ca0fd017f8a4e6abfbbab05ab
Killian Hourihan <kllnhrhn@gmail.com>
2026-01-15 15:13:47 -0800

    Add constants for joystick port and left and right motor channels

--- robot.py
+++ robot.py
@@ -1,15 +1,19 @@
 import wpilib
 import wpilib.drive
 
+JOYSTICK_PORT = 0
+LEFT_MOTOR_CHANNEL = 0
+RIGHT_MOTOR_CHANNEL = 1
+
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
-        leftMotor = wpilib.PWMSparkMax(0)
-        rightMotor = wpilib.PWMSparkMax(1)
-        self.robotDrive = wpilib.drive.DifferentialDrive(leftMotor, rightMotor)
-        self.stick = wpilib.Joystick(0)
+        leftMotor = wpilib.PWMSparkMax(LEFT_MOTOR_CHANNEL)
+        rightMotor = wpilib.PWMSparkMax(RIGHT_MOTOR_CHANNEL)
+        self.drive = wpilib.drive.DifferentialDrive(leftMotor, rightMotor)
+        self.stick = wpilib.Joystick(JOYSTICK_PORT)
 
         rightMotor.setInverted(True)
 
     def teleopPeriodic(self):
-        self.robotDrive.arcadeDrive(self.stick.getY(), self.stick.getX())
+        self.drive.arcadeDrive(self.stick.getY(), self.stick.getX())
