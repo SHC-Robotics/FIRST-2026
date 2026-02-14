08c61209d75e03a1613ece0df3f26e7bb301c032
26cluu <26cluu@shcp.edu>
2026-02-13 17:39:03 -0800

    started prototyping P loop for driving straighter

--- subsystems/candrivesubsystem.py
+++ subsystems/candrivesubsystem.py
@@ -18,6 +18,16 @@ class CANDriveSubsystem(commands2.Subsystem):
         # Create the configuration to apply to motors. Voltage compensation helps
         # the robot perform more similarly on different battery voltages.
         config = configs.TalonFXConfiguration()
+
+        slot0 = config.slot0
+        slot0.k_p = 0.3
+        self.MAX_RPS = 70
+        self.leftLeader.configurator.apply(config)
+        self.rightLeader.configurator.apply(config)
+
+        self.velocity_request = controls.VelocityVoltage(0)
+
+        config.motor_output.neutral_mode = signals.NeutralModeValue.BRAKE
         config.motor_output.inverted = (
             configs.config_groups.InvertedValue.CLOCKWISE_POSITIVE
         )
@@ -43,5 +53,19 @@ class CANDriveSubsystem(commands2.Subsystem):
 
     def driveArcade(self, xSpeed: float, zRotation: float) -> None:
         xSpeed = -xSpeed
-        self.leftLeader.set_control(self.leftOut.with_output(xSpeed + zRotation))
-        self.rightLeader.set_control(self.rightOut.with_output(xSpeed - zRotation))
+
+        left_percent = xSpeed + zRotation
+        right_percent = xSpeed - zRotation
+
+        max_input = max(abs(left_percent), abs(right_percent), 1.0)
+        left_normalized = left_percent / max_input
+        right_normalized = right_percent / max_input
+
+        left_target_rps = left_normalized * self.MAX_RPS
+        right_target_rps = right_normalized * self.MAX_RPS
+
+        #self.leftLeader.set_control(self.leftOut.with_output(xSpeed + zRotation))
+        #self.rightLeader.set_control(self.rightOut.with_output(xSpeed - zRotation))
+
+        self.leftLeader.set_control(self.velocity_request.with_velocity(left_target_rps))
+        self.rightLeader.set_control(self.velocity_request.with_velocity(right_target_rps))
