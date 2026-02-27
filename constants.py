26b7cb4534ac0744985a6913a1579c72d00fbc82
26cluu <26cluu@shcp.edu>
2026-02-27 09:35:14 -0800

    working shoota

--- constants.py
+++ constants.py
@@ -22,11 +22,11 @@ class FuelConstants:
     # Voltage values for various fuel operations. These values may need to be tuned
     # based on exact robot construction.
     # See the Software Guide for tuning information
-    INTAKING_FEEDER_VOLTAGE = -12. * 0.1
-    INTAKING_INTAKE_VOLTAGE = 8.0 * 0.1
-    LAUNCHING_FEEDER_VOLTAGE = 9.0 * 0.1
-    LAUNCHING_LAUNCHER_VOLTAGE = 12 * 0.1
-    SPIN_UP_FEEDER_VOLTAGE = -6.0 * 0.1
+    INTAKING_FEEDER_VOLTAGE = -12. 
+    INTAKING_INTAKE_VOLTAGE = 8.0 
+    LAUNCHING_FEEDER_VOLTAGE = 9.0 
+    LAUNCHING_LAUNCHER_VOLTAGE = 12 
+    SPIN_UP_FEEDER_VOLTAGE = -6.0 
     SPIN_UP_SECONDS = 1.0
 
 
