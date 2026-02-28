4cb37adf372e588c17519131a0c2433dce803cbf
Killian Hourihan <kllnhrhn@gmail.com>
2026-02-27 16:41:00 -0800

    Add manual climb down command

--- robotcontainer.py
+++ robotcontainer.py
@@ -10,7 +10,7 @@ from commands.drive import Drive
 from commands.eject import Eject
 from commands.exampleauto import ExampleAuto
 from commands.intake import Intake
-from commands.climb_down import ClimbDown
+from commands.climb_down import ClimbDown, ClimbDownManual
 from commands.climb_up import ClimbUp
 from commands.launchsequence import LaunchSequence
 from subsystems.candrivesubsystem import CANDriveSubsystem
@@ -95,11 +95,9 @@ class RobotContainer:
         self.driverController.leftBumper().onTrue(ClimbDown(self.climbSubsystem))
         self.driverController.rightBumper().onTrue(ClimbUp(self.climbSubsystem))
 
+        self.driverController.y().whileTrue(ClimbDownManual(self.climbSubsystem))
+
         self.fuelSubsystem.run(lambda: self.fuelSubsystem.stop())
-
-
-
-
 
     def getAutonomousCommand(self) -> commands2.Command:
         return self.autoChooser.getSelected()
