966dc7fd6354a0c195df0ffd1831be36194903fa
Killian Hourihan <kllnhrhn@gmail.com>
2026-03-15 08:36:25 -0700

    Remove front vision camera and aim command

--- robotcontainer.py
+++ robotcontainer.py
@@ -9,7 +9,6 @@ import commands2.cmd
 from constants import OperatorConstants
 from commands.drive import Drive
 from commands.eject import Eject
-from commands.aim import Aim
 from commands.intake import Intake
 from commands.climb_down import ClimbDown
 from commands.climb_up import ClimbUp
@@ -48,14 +47,14 @@ class RobotContainer:
         self.climbSubsystem = CANClimbSubsystem()
 
         self.visionLocalizer = VisionLocalizer(self.driveSubsystem)
-        self.frontVisionCamera = VisionCamera("limelight-front")
+        # self.frontVisionCamera = VisionCamera("limelight-front")
         self.backVisionCamera = VisionCamera("limelight-back")
-        self.visionLocalizer.addCamera(
-            self.frontVisionCamera,
-            poseOnRobot=Translation3d(0.3066, 0.1056, 0.66), # Forward: +X, Right: +Y, Up: +Z
-            headingOnRobot=Rotation2d(0.0), # yaw
-            pitchAngleDegrees=0.0,
-        )
+        # self.visionLocalizer.addCamera(
+        #     self.frontVisionCamera,
+        #     poseOnRobot=Translation3d(0.3066, 0.1056, 0.66), # Forward: +X, Right: +Y, Up: +Z
+        #     headingOnRobot=Rotation2d(0.0), # yaw
+        #     pitchAngleDegrees=0.0,
+        # )
         self.visionLocalizer.addCamera(
             self.backVisionCamera,
             poseOnRobot=Translation3d(0.264, 0.094, 0.58),
@@ -71,7 +70,7 @@ class RobotContainer:
         # Run the homing routine immediately on startup to zero the climber encoders
         #ClimberHoming(self.climbSubsystem).schedule()
 
-        NamedCommands.registerCommand("Aim", Aim(self.driveSubsystem, self.driverController, self.frontVisionCamera))
+        # NamedCommands.registerCommand("Aim", Aim(self.driveSubsystem, self.driverController, self.frontVisionCamera))
 
         # The autonomous chooser
         self.autoChooser = AutoBuilder.buildAutoChooser()
@@ -85,7 +84,7 @@ class RobotContainer:
         # While the Y button on the operator controller is held, run the launch
         # sequence command on the fuel subsystem.
         self.operatorController.y().onTrue(
-            LaunchSequence(self.fuelSubsystem, self.frontVisionCamera)
+            LaunchSequence(self.fuelSubsystem)
         )
 
         self.operatorController.x().onTrue(StopLaunch(self.fuelSubsystem))
@@ -107,7 +106,7 @@ class RobotContainer:
         # factory with the values provided by the joystick axes on the driver
         # controller.
 
-        self.driverController.a().whileTrue(Aim(self.driveSubsystem, self.driverController, self.frontVisionCamera))
+        # self.driverController.a().whileTrue(Aim(self.driveSubsystem, self.driverController, self.frontVisionCamera))
 
         self.driveSubsystem.setDefaultCommand(
             Drive(self.driveSubsystem, self.driverController)
