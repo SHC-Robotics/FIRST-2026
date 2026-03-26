4d27b07b6ac1bab6431b21676f8a5b75a9f626c3
Killian Hourihan <kllnhrhn@gmail.com>
2026-03-25 19:43:40 -0700

    comment right climber

--- commands/climb_down.py
+++ commands/climb_down.py
@@ -28,8 +28,8 @@ class ClimbDown(commands2.Command):
         if self.climbSubsystem.isLeftAtPosition(self.targetPosition, ClimberConstants.POSITION_TOLERANCE):
             self.climbSubsystem.stopLeft()
 
-        if self.climbSubsystem.isRightAtPosition(self.targetPosition, ClimberConstants.POSITION_TOLERANCE):
-            self.climbSubsystem.stopRight()
+        # if self.climbSubsystem.isRightAtPosition(self.targetPosition, ClimberConstants.POSITION_TOLERANCE):
+        #     self.climbSubsystem.stopRight()
 
 
     def end(self, interrupted: bool) -> None:
